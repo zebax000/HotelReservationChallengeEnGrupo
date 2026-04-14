860888454ee5052b69338d463d3b2ee291bf15f8
juanCHolguin <jholguin887@soyudemedellin.edu.co>
2026-04-13 19:31:57 -0500

    Implementacion class Guest Implementación class Reservation

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -14,9 +14,32 @@ class Guest:
     VIP: str = 'Vip'
     name: str = 'Name'
     email: str = 'Email'
+    type_: str = field(default = REGULAR)
 
+    def __str__(self) -> str:
+        return f"Guest{self.name}, ({self.email}) of type {self.type_}"
 
 # TODO: Implement Reservation class here
+class Reservation:
+    guest_name: str
+    description: str
+    check_in: date
+    check_out: date
+    guest: list[Guest] = field(init =  False, default_factory = list)
+    id: str = field(default_factory = generate_unique_id())
+
+    def add_guest(self, name, email, type_=Guest.REGULAR):
+        guest = Guest(name, email, type_)
+        self.guest.append(guest)
+        return guest
+    def delete_guest(self,guest_index = int):
+        if self.id
+
+
+
+
+
+
 
 
 # TODO: Implement Room class here
