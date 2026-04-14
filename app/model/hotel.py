839bca1b8a61bea0bfe850dbd64482a3b53707e6
andres <a.camilo0806@gmail.com>
2026-04-13 19:13:21 -0500

    commit code class room complete

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -49,7 +49,27 @@ class Room:
         for booked_date in dates_to_book:
             self.availability[booked_date] = reservation_id
 
+    def release(self, reservation_id: str):
+        released = False
+        for d, saved_id in self.availability.items():
+            if saved_id == reservation_id:
+                self.availability[d] = None
+                released = True
+        if not released:
+            reservation_not_found_error()
 
+    def update_booking(self, reservation_id: str, check_in: date, check_out: date):
+        for d in self.availability:
+            if self.availability[d] == reservation_id:
+                self.availability[d] = None
+
+        current = check_in
+        while current < check_out:
+            if self.availability.get(current) is not None:
+                room_not_available_error()
+            else:
+                self.availability[current] = reservation_id
+            current += timedelta(days=1)
 
 
 # TODO: Implement Hotel class here
